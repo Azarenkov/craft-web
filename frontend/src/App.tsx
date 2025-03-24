9d7d0c3a0f8e01c3ae455ae099542a2e6a94a04d
Alexey <azarenkov20141@icloud.com>
2025-03-24 16:38:28 +0500

    feat: impl of frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// frontend/src/App.tsx
+import React from "react";
+import SignInSide from "./sign-in-side/SignInSide";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <span>ASCII Art Generator</span>
+        </nav>
       </header>
+      <SignInSide />
     </div>
   );
-}
+};
 
 export default App;
